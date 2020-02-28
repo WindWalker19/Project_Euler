f1ee7ec01839178e7550e7121a3bc69266605d07
Sonam sherpa <sherpasonam44@hotmail.com>
2020-02-27 21:07:19 -0800

    Add files via upload

---
+++ head.h
@@ -0,0 +1,5 @@
+// It contains all the header required to resolve the project euler problems.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
---
+++ pro.c
@@ -0,0 +1,18 @@
+//It contains all the solution of project euler in c.
+
+#include "head.h"
+#include "pro1.h"
+#include "pro2.h"
+
+
+int main(){
+
+	int answer1 = proj1();
+	printf("The solution of the first problem : %d\n",answer1 );
+	
+	int answer2 = proj2();
+	printf("The solution of the first problem : %d\n",answer2 );
+	
+
+	return 0;
+}
